ce99b909bdc59ea3ecbdf5aca5f418bfa4539d9a
Petr Štipek <p.stipek@email.cz>
2016-02-03 15:02:30 +0100

    Ignore missing bower json for depending modules

--- src/Modules/Composer/Extra/Bower.php
+++ src/Modules/Composer/Extra/Bower.php
@@ -58,7 +58,7 @@ class Bower implements IExtra
 		if (isset($extra['bower']['files']) AND is_array($extra['bower']['files'])) {
 			foreach ($extra['bower']['files'] as $file) {
 				$path = getcwd() . "/" . $file;
-				if (!is_file($path)) throw new \OutOfRangeException("File $file defined in composer section extra.bower.files does not exist on path: $path");
+				if (!is_file($path) AND $isMain) throw new \OutOfRangeException("File $file defined in composer section extra.bower.files does not exist on path: $path");
 				$bower = json_decode(file_get_contents($path));
 				if (isset($bower->dependencies)) {
 					foreach ($bower->dependencies as $package => $version) {
